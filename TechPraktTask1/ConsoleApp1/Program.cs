396b5c45f564325f576a20d975fb214c71f4b668
Volodymyr Zakusylo <ipz221_zvv@student.ztu.edu.ua>
2024-02-17 16:03:51 +0200

    Додано консольний додаток та gitignore

---
+++ TechPraktTask1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть перше число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введіть друге число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double sum = num1 + num2;
+        Console.WriteLine($"Сума чисел {num1} і {num2} дорівнює {sum}");
+    }
+}
